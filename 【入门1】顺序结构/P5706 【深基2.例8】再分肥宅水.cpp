fc1a73653ff0ad749a3d918729627c3e54c0177f
JustinJia19 <142551939+JustinJia19@users.noreply.github.com>
2025-04-03 09:00:30 +0800

    Create P5706 【深基2.例8】再分肥宅水.cpp

---
+++ 【入门1】顺序结构/P5706 【深基2.例8】再分肥宅水.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    float t;
+    int n;
+    cin >> t>> n;
+    float x = t/n;
+    int y = n*2;
+    printf("%.3f",x);
+    cout<<endl<<y;
+}
