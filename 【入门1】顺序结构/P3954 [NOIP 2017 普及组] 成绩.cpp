7befb1d7da5eb5211edeff5a6227c616ca0f09ea
JustinJia19 <142551939+JustinJia19@users.noreply.github.com>
2025-04-03 09:04:38 +0800

    Create P3954 [NOIP 2017 普及组] 成绩.cpp

---
+++ 【入门1】顺序结构/P3954 [NOIP 2017 普及组] 成绩.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{   
+    int h1,h2,m1;
+    cin >>h1>>m1>>h2;
+    int t = h1*0.2+m1*0.3+h2*0.5;
+
+
+    cout <<t;
+
+    return 0;
+}
