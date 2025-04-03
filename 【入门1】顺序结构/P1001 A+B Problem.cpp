537825da60af76e55cf80be2175953b7b177a159
JustinJia19 <142551939+JustinJia19@users.noreply.github.com>
2025-04-03 08:56:53 +0800

    Create P1001 A+B Problem.cpp

---
+++ 【入门1】顺序结构/P1001 A+B Problem.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int long long a;
+    int long long b;
+    cin >> a>> b;
+    int long long c = a + b;
+    cout <<c;
+    return 0;
+}
