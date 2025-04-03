04df0556de9027a1247297bc3dcc6b3961f00ed9
JustinJia19 <142551939+JustinJia19@users.noreply.github.com>
2025-04-03 09:11:50 +0800

    Create P5712 【深基3.例4】Apples.cpp

---
+++ 【入门2】分支结构/P5712 【深基3.例4】Apples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>=2)
+    cout<<"Today, I ate "<<n<<" apples.";
+    else
+    cout<<"Today, I ate "<<n<<" apple.";
+
+    return 0;
+}
